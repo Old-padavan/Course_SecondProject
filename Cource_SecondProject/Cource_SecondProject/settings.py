42d8eaa5a356cdbd881ca3f1ee020bb97fd3818f
Alexandr Ratmanov <al.ratmanov@gmail.com>
2023-07-04 10:48:36 +0300

    Изменил views.py, создал templates, обновил DIRS в settings.py, создал home.html

--- Cource_SecondProject/Cource_SecondProject/settings.py
+++ Cource_SecondProject/Cource_SecondProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Cource_SecondProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
